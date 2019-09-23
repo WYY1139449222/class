d039786b8f4fb6fda69cc86feba647c06cc87dd4
WYY1139449222 <54354280+WYY1139449222@users.noreply.github.com>
2019-09-23 09:35:25 +0800

    阿达

--- week2/华为案例/js/index.js
+++ week2/华为案例/js/index.js
@@ -1,58 +1,65 @@
-function HTML(obj) {
-    let lis = document.createElement('li');
-    let img = document.createElement('img');
-    let p1 = document.createElement('p');
-    let p2 = document.createElement('p');
-    let p3 = document.createElement('p');
-    let div1 = document.createElement('div');
-    let div2 = document.createElement('div');
-
-    for (let key in obj) {
-        if(key == 'img'){
-            img.src = obj[key];
-        }else if(key == 'p1'){
-            p1.innerText = obj[key];
-        }else if(key == 'p2'){
-            p2.innerText = obj[key];
-        }else if(key == 'p3'){
-            p3.innerText = `￥${obj[key]}`;
-        }else if(key == 'div2'){
-            div2.innerText = `${obj[key]}评价`;
-        }
+let dataAry;
+// 把数据从后台获取到 然后渲染到页面上
+let xhr = new XMLHttpRequest();
+xhr.open('get', './data.json', false);
+xhr.onreadystatechange = function () {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+        // 代表请求成功；
+        dataAry = JSON.parse(xhr.response); // 把从后台获取到的JSON字符串转化成 JSON对象
     }
-    div1.innerText = '选购'
-    lis.appendChild(img)
-    lis.appendChild(p1)
-    lis.appendChild(p2)
-    lis.appendChild(p3)
-    lis.appendChild(div1)
-    lis.appendChild(div2)
-    return lis
 }
-let obj = {
-    img: 'images/n1.png',
-    p1: 'HUAWEI Mate 20 Pro',
-    p2: '赠好礼|享12期免息',
-    p3: 9999,
-    div2: 21424
+xhr.send();
+let body = document.getElementById('body')
+function HTML(ary) {
+    let str = '';
+    ary.forEach(item => {
+        let {
+            img,
+            title,
+            num,
+            price,
+            time
+        } = item;
+        str += `<li>
+        <img src="${img}" alt="" title="${title}">
+        <p>${title}</p>
+        <p>${time}</p>
+        <p>￥${price}</p>
+        <div>选购</div>
+        <div>${num}评价</div>
+        </li>`
+    });
+    body.innerHTML = str;
 }
-let ary=[
-            { img: 'images/n1.png',
-            p1: 'HUAWEI Mate 20 Pro',
-            p2: '赠好礼|享12期免息',
-            p3: 9999,
-            div2: 21424
-            },
-            { img: 'images/n2.png',
-            p1: 'HUAWEI Mate 20 Pro',
-            p2: '赠好礼|享12期免息',
-            p3: 9999,
-            div2: 21424}
-        ]
-let ul = document.getElementById('body');
-let ary = []
-for (let i = 0; i < 5; i++) {
-    ary[i] = HTML(obj)
-    ul.appendChild(ary[i]);
+HTML(dataAry);
+let time = document.getElementById('time');
+let price = document.getElementById('price');
+let pj = document.getElementById('pj');
+time.a = price.a =pj.a = 1;
+
+function btn(nn){
+    return function(){
+        this.a*=-1;
+        dataAry.sort((a,b)=>(a[nn]-b[nn])*this.a)
+        HTML(dataAry);
+    }
 }
+time.onclick =btn('time')
+price.onclick = btn('price')
+pj.onclick = btn('num')
+// time.onclick = function(){
+//     this.a*=-1;
+//     dataAry.sort((a,b)=>(a.time-b.time)*this.a)
+//     HTML(dataAry);
+// }
+// price.onclick = function(){
+//     this.a*=-1;
+//     dataAry.sort((a,b)=>(a.price-b.price)*this.a)
+//     HTML(dataAry);
+// }
+// pj.onclick = function(){
+//     this.a*=-1;
+//     dataAry.sort((a,b)=>(a.num-b.num)*this.a)
+//     HTML(dataAry);
+// }
 
